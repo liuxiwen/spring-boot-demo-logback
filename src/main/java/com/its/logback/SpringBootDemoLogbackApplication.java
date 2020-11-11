c6bdae0c5eb5573182060bfaa4f22d01556c53cf
liuxiwen <xiwenhehe@sina.com>
2020-11-11 10:48:10 +0800

    feat(*): "init"

---
+++ src/main/java/com/its/logback/SpringBootDemoLogbackApplication.java
@@ -0,0 +1,13 @@
+package com.its.logback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoLogbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDemoLogbackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/its/logback/SpringBootDemoLogbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.its.logback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoLogbackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
