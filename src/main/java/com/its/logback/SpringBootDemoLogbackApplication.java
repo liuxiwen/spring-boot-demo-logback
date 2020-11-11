45a1015027c8ff2fc1abfbf405d68858de92c008
liuxiwen <xiwenhehe@sina.com>
2020-11-11 10:50:59 +0800

    fix: 修改入口文件

--- src/main/java/com/its/logback/SpringBootDemoLogbackApplication.java
+++ src/main/java/com/its/logback/SpringBootDemoLogbackApplication.java
@@ -3,6 +3,9 @@ package com.its.logback;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Aaron
+ */
 @SpringBootApplication
 public class SpringBootDemoLogbackApplication {
 
