9bf11c41177581ce9ca40ec08542158a5b790955
liuxiwen <xiwenhehe@sina.com>
2020-11-11 10:55:28 +0800

    fix: 修改

--- src/main/java/com/its/logback/SpringBootDemoLogbackApplication.java
+++ src/main/java/com/its/logback/SpringBootDemoLogbackApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
+ * @date 2020/11/11
  * @author Aaron
  */
 @SpringBootApplication
